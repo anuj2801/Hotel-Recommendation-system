4d69c7f3a6265cb95eb7055362f5da75223cec3f
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2023-04-08 18:00:03 +0530

    Fetch hotels from backend and show on frontend

--- Hotel Recommendation system/Frontend/src/components/searchItem/SearchItem.jsx
+++ Hotel Recommendation system/Frontend/src/components/searchItem/SearchItem.jsx
@@ -1,33 +1,42 @@
 import { Link } from "react-router-dom";
 import "./searchItem.css";
+import { useEffect, useState } from "react";
 
 const SearchItem = ({ item }) => {
+  const [price,setprice] = useState(0);
+  const [distance,setdistance] = useState(0);
+  useEffect(()=>{
+    let x = Math.floor((Math.random() * 1300) + 700);
+    setprice(x);
+    let y = Math.floor((Math.random() * 4) + 1);
+    setdistance(y);
+  },[])
   return (
     <div className="searchItem">
-      <img src={item.photos[0]} alt="" className="siImg" />
+      <img src={"https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="} alt="" className="siImg" />
       <div className="siDesc">
-        <h1 className="siTitle">{item.name}</h1>
-        <span className="siDistance">{item.distance}m from center</span>
+        <h1 className="siTitle">{item.property_name}</h1>
+        <span className="siDistance">{distance} Km from your location</span>
         <span className="siTaxiOp">Free airport taxi</span>
         <span className="siSubtitle">
-          Studio Apartment with Air conditioning
+          {item.in_your_room}
         </span>
-        <span className="siFeatures">{item.desc}</span>
+        {/* <span className="siFeatures">Good Hotel with all facilities</span> */}
         <span className="siCancelOp">Free cancellation </span>
         <span className="siCancelOpSubtitle">
           You can cancel later, so lock in this great price today!
         </span>
       </div>
       <div className="siDetails">
-        {item.rating && <div className="siRating">
-          <span>Excellent</span>
-          <button>{item.rating}</button>
+        {item.hotel_star_rating && <div className="siRating">
+          {/* <span>Excellent</span> */}
+          <button>{item.hotel_star_rating} ★</button>
         </div>}
         <div className="siDetailTexts">
-          <span className="siPrice">${item.cheapestPrice}</span>
+          <span className="siPrice">₹ {price}</span>
           <span className="siTaxOp">Includes taxes and fees</span>
-          <Link to={`/hotels/${item._id}`}>
-          <button className="siCheckButton">See availability</button>
+          <Link to={`/hotels/${item.uniq_id}`}>
+            <button className="siCheckButton">See availability</button>
           </Link>
         </div>
       </div>
--- Hotel Recommendation system/Frontend/src/pages/list/List.jsx
+++ Hotel Recommendation system/Frontend/src/pages/list/List.jsx
@@ -12,134 +12,69 @@ import axios from "axios";
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
-  // const [dates, setDates] = useState(location.state.dates);
-  // const [openDate, setOpenDate] = useState(false);
-  // const [options, setOptions] = useState(location.state.options);
-  // const [min, setMin] = useState(undefined);
-  // const [max, setMax] = useState(undefined);
+  const [dates, setDates] = useState(location.state.dates);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
 
-  // const { data, loading, error, reFetch } = useFetch(
-  //   `/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
-  // );
-
-  // const handleClick = () => {
-  //   reFetch();
-  // };
 
   const [cityes, setcityes] = useState([]);
 
-  const fetchHotel=async()=>{
+  const fetchHotel = async () => {
     try {
-      const res = await axios.get("http://localhost:8800/api/hotels/"+destination);
+      console.log('search for destination =>',destination);
+      const res = await axios.get("http://localhost:8800/api/hotels/" + destination);
       setcityes(res.data);
-      // console.log('all hotel',res);
+      console.log('all hotel',res);
     } catch (error) {
       console.log(error);
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchHotel();
-  },[])
+  }, [])
 
   return (
     <div>
       <Navbar />
-      {
-        cityes.length>0 && cityes.map((e)=>{
-          return <h1>{e.property_name}</h1>
-        })
-      }
-      {/* <Header type="list" />
+      <Header type="list" />
       <div className="listContainer">
         <div className="listWrapper">
           <div className="listSearch">
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
               <label>Destination</label>
-              <input placeholder={destination} type="text" />
-            </div>
-            <div className="lsItem">
-              <label>Check-in Date</label>
-              <span onClick={() => setOpenDate(!openDate)}>{`${format(
-                dates[0].startDate,
-                "MM/dd/yyyy"
-              )} to ${format(dates[0].endDate, "MM/dd/yyyy")}`}</span>
-              {openDate && (
-                <DateRange
-                  onChange={(item) => setDates([item.selection])}
-                  minDate={new Date()}
-                  ranges={dates}
-                />
-              )}
+              <input placeholder={destination} onChange={(e) => setDestination(e.target.value)} type="text" />
             </div>
             <div className="lsItem">
               <label>Options</label>
               <div className="lsOptions">
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">
-                    Min price <small>per night</small>
-                  </span>
-                  <input
-                    type="number"
-                    onChange={(e) => setMin(e.target.value)}
-                    className="lsOptionInput"
-                  />
-                </div>
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">
-                    Max price <small>per night</small>
-                  </span>
-                  <input
-                    type="number"
-                    onChange={(e) => setMax(e.target.value)}
-                    className="lsOptionInput"
-                  />
-                </div>
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">Adult</span>
-                  <input
-                    type="number"
-                    min={1}
-                    className="lsOptionInput"
-                    placeholder={options.adult}
-                  />
-                </div>
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">Children</span>
-                  <input
-                    type="number"
-                    min={0}
-                    className="lsOptionInput"
-                    placeholder={options.children}
-                  />
-                </div>
-                <div className="lsOptionItem">
+              </div>
+            </div>
+            <div className="lsOptionItem">
                   <span className="lsOptionText">Room</span>
                   <input
                     type="number"
                     min={1}
                     className="lsOptionInput"
-                    placeholder={options.room}
+                    // placeholder={options.room}
                   />
                 </div>
-              </div>
-            </div>
-            <button onClick={handleClick}>Search</button>
+            <button onClick={fetchHotel}>Search</button>
           </div>
           <div className="listResult">
-            {loading ? (
-              "loading"
-            ) : (
+            {(
               <>
-                {data.map((item) => (
+                {cityes.length>0 ? cityes.map((item) => (
                   <SearchItem item={item} key={item._id} />
-                ))}
+                )):<h1 style={{'textAlign':'center'}}>No Hotels</h1>}
               </>
             )}
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
